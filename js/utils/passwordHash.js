ec5058ef288eebf422269d5a90bc761d35f932b0
Chimmy <chimmy@chimmy.xyz>
2026-01-16 13:35:52 +0800

    fix simple bug password not being salted due to incorect variable

--- js/utils/passwordHash.js
+++ js/utils/passwordHash.js
@@ -3,7 +3,7 @@ const SALT = '1Av7kHTspF2uVCve6wS460cBteeRH3IZ'
 
 export async function hashPassword(password) {
     const encoder = new TextEncoder();
-    const data = encoder.encode(password + salt);
+    const data = encoder.encode(password + SALT);
     const hashBuffer = await crypto.subtle.digest('SHA-256', data);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
@@ -14,7 +14,6 @@ export async function hashPassword(password) {
  * Verify password against stored hash
  * @param {string} password - Password to verify
  * @param {string} hash - Stored hash
- * @param {string} salt - Salt used in hashing
  * @returns {Promise<boolean>} Is password correct
  */
 export async function verifyPassword(password, hash) {
